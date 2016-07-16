df31a12a055470ca6b94576a6065043fb9b528fb
Michael Svit <michaelsvit@gmail.com>
2016-07-16 18:55:54 +0300

    Bind WebView to XML for consistent HTML fetching

--- app/src/main/java/com/michaelsvit/kolnoa/MovieGridFragment.java
+++ app/src/main/java/com/michaelsvit/kolnoa/MovieGridFragment.java
@@ -1,7 +1,6 @@
 package com.michaelsvit.kolnoa;
 
 import android.content.Context;
-import android.net.http.SslError;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -31,7 +29,9 @@ public class MovieGridFragment extends Fragment {
 
     private int columnCount = 2;
     private Cinema cinema;
-    private List<Movie> movies;
+
+    ArrayList<Movie> movies;
+    MovieRecyclerViewAdapter adapter;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -55,10 +55,6 @@ public class MovieGridFragment extends Fragment {
         if (getArguments() != null) {
             cinema = getArguments().getParcelable(ARG_CINEMA);
         }
-
-        movies = new ArrayList<>();
-        // TODO: remove
-        fetchMovies();
     }
 
     @Override
@@ -66,40 +62,33 @@ public class MovieGridFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_movies_grid, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.setLayoutManager(new GridLayoutManager(context, columnCount));
-            recyclerView.setAdapter(new MovieRecyclerViewAdapter(movies));
-        }
+        Context context = view.getContext();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_movies_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(context, columnCount));
+        movies = new ArrayList<>();
+        adapter = new MovieRecyclerViewAdapter(movies);
+        recyclerView.setAdapter(adapter);
+
+        fetchMovies(view);
+
         return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        //fetchMovies();
-    }
-
-    private void fetchMovies() {
+    private void fetchMovies(View view) {
         class JSInterface{
             @JavascriptInterface
             public void processHTML(String html){
-                populateMovies(html);
+                if(html.length() > 10000){
+                    new ParseHTMLResponse().execute(html);
+                }
             }
         }
-        final WebView webView = new WebView(getActivity());
+        final WebView webView = (WebView) view.findViewById(R.id.fragment_movies_web_view);
         WebSettings settings = webView.getSettings();
         settings.setDomStorageEnabled(true);
         webView.addJavascriptInterface(new JSInterface(), "Android");
-
+        webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
@@ -109,32 +98,28 @@ public class MovieGridFragment extends Fragment {
                 }
             }
         });
-        webView.setWebChromeClient(new WebChromeClient());
+
         settings.setJavaScriptEnabled(true);
-        //webView.clearCache(true);
-        //webView.clearHistory();
         webView.loadUrl(cinema.getUrl().toString());
-
     }
 
-    private void populateMovies(String html) {
-        Log.d("TEST", html);
-    }
-
-    private class FetchMoviesTask extends AsyncTask<Cinema, Void, String>{
-        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+    private class ParseHTMLResponse extends AsyncTask<String, Void, List<Movie>> {
+        private final String LOG_TAG = ParseHTMLResponse.class.getSimpleName();
 
         @Override
-        protected String doInBackground(Cinema... cinemas) {
-            // TODO: check internet connection
+        protected void onPostExecute(List<Movie> movies) {
+            movies.addAll(movies);
+            adapter.notifyItemRangeInserted(0, movies.size());
+        }
 
-            /*try {
-                Document document = Jsoup.connect("http://www.yesplanet.co.il/movies").get();
-                Log.d("TEST", document.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-            return null;
+        @Override
+        protected List<Movie> doInBackground(String... params) {
+            if(params.length > 0) {
+                return cinema.getMoviesFromHTML(params[0]);
+            } else {
+                Log.e(LOG_TAG, "Error parsing HTML.");
+                return null;
+            }
         }
     }
 }
