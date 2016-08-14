5495a161e74bfb4ef4aa4eccb167238173fea427
Michael Svit <michaelsvit@gmail.com>
2016-08-14 16:34:54 +0300

    Set details activity design

--- app/src/main/java/com/michaelsvit/kolnoa/MovieDetailsActivity.java
+++ app/src/main/java/com/michaelsvit/kolnoa/MovieDetailsActivity.java
@@ -1,18 +1,27 @@
 package com.michaelsvit.kolnoa;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    private Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        movie = getIntent().getParcelableExtra(Movie.ARG_NAME);
+
         setContentView(R.layout.activity_movie_details);
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolbarLayout.setTitle(movie.getTitle());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +33,12 @@ public class MovieDetailsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        TextView textView = (TextView) findViewById(R.id.details_synopsis);
+        textView.setText(movie.getSynopsis());
     }
 }
