7af20286d3e5bbd7a3c05f0c81881a81bd79fd48
Michael Svit <michaelsvit@gmail.com>
2016-08-21 16:35:19 +0300

    Remove MovieScreeningsActivity FAB

--- app/src/main/java/com/michaelsvit/kolnoa/MovieScreeningsActivity.java
+++ app/src/main/java/com/michaelsvit/kolnoa/MovieScreeningsActivity.java
@@ -2,12 +2,9 @@ package com.michaelsvit.kolnoa;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,14 +26,6 @@ public class MovieScreeningsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if(savedInstanceState == null){
