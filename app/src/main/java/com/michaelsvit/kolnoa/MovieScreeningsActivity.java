29ce496511712aaf79674fbe0895a1688bf42f75
Michael Svit <michaelsvit@gmail.com>
2016-08-23 14:06:07 +0300

    Move title text to a TextView instead of default property, and sort schedule list

--- app/src/main/java/com/michaelsvit/kolnoa/MovieScreeningsActivity.java
+++ app/src/main/java/com/michaelsvit/kolnoa/MovieScreeningsActivity.java
@@ -3,10 +3,13 @@ package com.michaelsvit.kolnoa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +29,11 @@ public class MovieScreeningsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowTitleEnabled(false);
+        }
 
         if(savedInstanceState == null){
             FragmentManager fragmentManager = getSupportFragmentManager();
@@ -40,6 +47,20 @@ public class MovieScreeningsActivity extends AppCompatActivity {
 
     private Map<String, List<MovieScreening>> splitToDays(List<MovieScreening> list) {
         Map<String, List<MovieScreening>> splitList = new HashMap<>();
+        Collections.sort(list, new Comparator<MovieScreening>() {
+            @Override
+            public int compare(MovieScreening s0, MovieScreening s1) {
+                String time0 = s0.getTime();
+                String hour0 = time0.split(":")[0];
+                String time1 = s1.getTime();
+                String hour1 = time1.split(":")[0];
+                if (hour0.equals("00") ^ hour1.equals("00")) {
+                    return time1.compareTo(time0);
+                } else {
+                    return time0.compareTo(time1);
+                }
+            }
+        });
 
         for(MovieScreening movieScreening : list){
             String date = movieScreening.getDate();
